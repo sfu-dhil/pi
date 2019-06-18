014f7e5bcfe35cfb6785ee9086ce143e748ab239
Michael Joyce <ubermichael@gmail.com>
2019-06-18 13:35:39 -0700

    Add a download keywords button to the keywords page.

--- src/AppBundle/Controller/KeywordController.php
+++ src/AppBundle/Controller/KeywordController.php
@@ -4,11 +4,14 @@ namespace AppBundle\Controller;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use AppBundle\Entity\Keyword;
 use AppBundle\Form\KeywordType;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
  * Keyword controller.
@@ -39,6 +42,40 @@ class KeywordController extends Controller {
     }
 
     /**
+     * Generates a summary of the youtube keyword usage.
+     *
+     * @Route("/download", name="keyword_download", methods={"GET"})
+     */
+    public function downloadAction() {
+        $em = $this->getDoctrine()->getManager();
+        $dql = 'SELECT e FROM AppBundle:Keyword e ORDER BY e.id';
+        $query = $em->createQuery($dql);
+        $iterator = $query->iterate();
+        $data = array();
+        $data[0] = array(
+            'id', 'keyword', 'count', 'url'
+        );
+        while($row = $iterator->next()) {
+            /** @var Keyword $keyword */
+            $keyword = $row[0];
+            $data[] = [
+                $keyword->getId(),
+                $keyword->getLabel(),
+                $keyword->getVideos()->count(),
+                $this->generateUrl('keyword_show', array('id' => $keyword->getId()), UrlGeneratorInterface::ABSOLUTE_URL),
+            ];
+            $em->detach($keyword);
+        }
+        $csv = $this->container->get('serializer')->encode($data, 'csv');
+        $response = new Response($csv, 200, ['Content-Type' => 'text/csv']);
+        $disposition = $response->headers->makeDisposition(
+            ResponseHeaderBag::DISPOSITION_ATTACHMENT,
+            'keyword-usage.csv');
+        $response->headers->set('Content-Disposition', $disposition);
+        return $response;
+    }
+
+    /**
      * Finds and displays a Keyword entity.
      *
      * @Route("/{id}", name="keyword_show", methods={"GET"})
@@ -52,5 +89,4 @@ class KeywordController extends Controller {
             'keyword' => $keyword,
         );
     }
-
 }
